5dd988184ceede87e7cb68d032ec5d7b7e737e73
cegs0612 <127986654+cegs0612@users.noreply.github.com>
2023-08-01 12:18:19 -0400

    general structure added

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-console.log("Hi everyone!")
+//I think we should stick to lowercase to return the results so it's easier
+
+function computerPlay() {
+    //should return "rock", "paper", or "scissors"
+}
+
+
+function playerPlay(/*round*/) {
+    // should return "rock", "paper", or "scissors" 
+    // I think it would be nice to show the player in the prompt what round is playing, so maybe it should get it as a parameter
+    // prevent the player to write anything anything different to rock, paper or scissors, also it shouldn't matter if they use lower or uppercase
+}
+
+function playRound(/*playerSelection,computerSelection*/) {
+    // should return who the winner is, player, computer or is a tie
+    // gets playerSelection, and computerSelection
+}
+
+function game() {
+    //it calls the other three functions
+    //provide round variable to playerPlay()
+    //gets "rock","paper" of "scissors" from playerPlay() and computerPlay()
+    //provide (playerSelection,computerSelection) in that order and as single variables, not object nor array, to playRound()   
+}
