d185c5106a5a621d377d4afc04bb5ac78d2d7224
mcbrm <39712982+mcbrm@users.noreply.github.com>
2023-08-01 22:34:34 +0100

    Update game.js

--- game.js
+++ game.js
@@ -9,6 +9,18 @@ function playerPlay(/*round*/) {
     // should return "rock", "paper", or "scissors" 
     // I think it would be nice to show the player in the prompt what round is playing, so maybe it should get it as a parameter
     // prevent the player to write anything anything different to rock, paper or scissors, also it shouldn't matter if they use lower or uppercase
+    // (NOTE: I am not sure if it should be the right way)
+    const options = ["rock", "paper", "scissors"];
+    let selectOption = prompt("Select: 'rock', 'paper', or 'scissors'");
+    selectOption = selectOption.toLowerCase();
+
+    while (!options.includes(selectOption)) {
+        selectOption = prompt("Invalid choice. Please, select: 'rock', 'paper', or 'scissors'");
+        selectOption = selectOption.toLowerCase();
+    }    
+    let round = 1;
+    return "The selected option is: " + selectOption + " ; and this is round: " + round;
+    round++
 }
 
 function playRound(/*playerSelection,computerSelection*/) {
